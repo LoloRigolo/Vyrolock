7cd36c6f5f80e8306c119689ba5b7c1ff19ef7e4
LoloRigolo <lolocritiqueurs21@gmail.com>
2025-03-31 14:59:37 +0200

    feat : ajout de fonction pour surveiller les initials access

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import IpAddressService
+import InitialAcessService
 import json
 from scapy.all import rdpcap, IP, TCP, UDP
 
@@ -25,16 +26,26 @@ filtered_packets = [extract_info(p) for p in packets if extract_info(p)]
 
 packets_json = json.dumps(filtered_packets, indent=4)
 
+result: dict = {}
+
 ips = []
 ips_analysis = {}
+initials_access_analysis = {}
 
 for packet_data in filtered_packets:
     ip = packet_data["dst_ip"]
     port = packet_data["port"]
+    if InitialAcessService.check_initial_acess(port):
+        if ip in initials_access_analysis:
+            initials_access_analysis[ip]["attempts"] += 1
+        else:
+            initial_access_analysis = { "port" : [port], "attempts": 1}
+            initials_access_analysis[ip] = initial_access_analysis
     
     if ip not in ips:
         ips.append(ip)
         ip_analysis = IpAddressService.check_sources(ip)
         ips_analysis[ip] = ip_analysis
 
-print(json.dumps(ips_analysis, indent=4))
+#result = {ips_analysis, initials_access_analysis}
+print(json.dumps(initials_access_analysis, indent=4))
