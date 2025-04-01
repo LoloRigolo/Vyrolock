4c72c57e278b2aa6d9ddddb0939e7915ec2a934b
LoloRigolo <lolocritiqueurs21@gmail.com>
2025-04-01 10:01:16 +0200

    feat: add a privilege escalation analysis fix : modify main.py to adapt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import IpAddressService
-import InitialAcessService
-import PersistenceService
+from IpAddressService import check_ip_address
+from InitialAcessService import check_initial_acess
+from PersistenceService import check_persistence
+from PrivilegeEscalationService import check_privilege_escalation
 import json
-from scapy.all import rdpcap, IP, TCP, UDP
+from scapy.all import rdpcap, IP, TCP, UDP, Raw
 from collections import defaultdict
 
 packets = rdpcap("ex4.pcap")
@@ -23,7 +24,11 @@ def extract_info(packet):
             data["port"] = packet[UDP].dport
         else:
             data["port"] = None
-        
+
+        if packet.haslayer(Raw):
+            data["payload"] = packet[Raw].load.decode(errors="ignore")
+        else:
+            data["payload"] = ""
         return data
     return None
 
@@ -38,28 +43,31 @@ ips_analysis = {}
 initials_access_analysis = {}
 c2_persistence_analysis = {}
 
+privilege_escalation_analysis: dict = check_privilege_escalation(filtered_packets)
+
 for packet_data in filtered_packets:
     ip = packet_data["dst_ip"]
     port = packet_data["port"]
     timestamp = packet_data["timestamp"]
 
-    initials_access_analysis = InitialAcessService.check_initial_acess(port, initials_access_analysis, ip)
+    initials_access_analysis = check_initial_acess(port, initials_access_analysis, ip)
      
     # if ip not in ips:
     #     ips.append(ip)
-    #     ip_analysis = IpAddressService.check_sources(ip)
+    #     ip_analysis = check_sources(ip)
     #     ips_analysis[ip] = ip_analysis
 
     connections[(packet_data["src_ip"], ip, port)].append(timestamp)
 
 # Persistence Service
 for (src_ip, dst_ip, dport), timestamps in connections.items():
-    c2_persistence_analysis = PersistenceService.check_persistence(timestamps, c2_persistence_analysis, dst_ip, dport)
+    c2_persistence_analysis = check_persistence(timestamps, c2_persistence_analysis, dst_ip, dport)
 
 result = {
     "ips_analysis": ips_analysis,
     "initials_access_analysis": initials_access_analysis,
-    "c2_persistence_analysis": c2_persistence_analysis
+    "c2_persistence_analysis": c2_persistence_analysis,
+    "privilege_escalation_analysis": privilege_escalation_analysis
 }
 
 print(json.dumps(result, indent=4, default=str))
